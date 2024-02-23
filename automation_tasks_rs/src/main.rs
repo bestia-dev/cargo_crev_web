859d727ac330086e76bc099e67c14c2c96cd0832
luciano_bestia <info@bestia.dev>
2024-02-23 12:05:12 -0500

    2024-02-23

--- automation_tasks_rs/src/main.rs
+++ automation_tasks_rs/src/main.rs
@@ -96,13 +96,13 @@ fn completion() {
 
     if last_word == "cargo-auto" || last_word == "auto" {
         let sub_commands = vec!["build", "release", "doc", "test", "commit_and_push", "publish_to_web"];
-        completion_return_one_or_more_sub_commands(sub_commands, word_being_completed);
+       cl::completion_return_one_or_more_sub_commands(sub_commands, word_being_completed);
     }
     /*
     // the second level if needed
     else if last_word == "new" {
         let sub_commands = vec!["x"];
-        completion_return_one_or_more_sub_commands(sub_commands, word_being_completed);
+       cl::completion_return_one_or_more_sub_commands(sub_commands, word_being_completed);
     }
     */
 }
