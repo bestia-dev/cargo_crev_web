8e5a8db7be8a885d2ed693d84f3c24a45297b137
bestia.dev <info@bestia.dev>
2025-03-31 13:52:56 +0200

    © 2025 bestia.dev

--- automation_tasks_rs/src/main.rs
+++ automation_tasks_rs/src/main.rs
@@ -73,7 +73,7 @@ fn print_help() {
     {YELLOW}(If you use SSH, it is easy to start the ssh-agent in the background and ssh-add your credentials for git.){RESET}
 {GREEN}cargo auto publish_to_web - publish to web, git tag{RESET}
 
-    {YELLOW}© 2024 bestia.dev  MIT License github.com/automation-tasks-rs/cargo-auto{RESET}
+    {YELLOW}© 2025 bestia.dev  MIT License github.com/automation-tasks-rs/cargo-auto{RESET}
 "#
     );
     print_examples_cmd();
